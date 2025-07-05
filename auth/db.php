239e83f637068388ecdb2da92419d5e6b5eadb3b
caklore <hacvutienlong05@gmail.com>
2025-07-05 14:23:26 +0700

    testdb

--- auth/db.php
+++ auth/db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "WEBTRUYENDS";
+$dbname = "webtruyends";
 $port = 3307;
 
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
